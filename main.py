c36a6c0a0d466573c4a6e4ea84d3b43026749c7a
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2025-01-26 11:53:34 +0000

    add char counter method

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ def main():
     frankenstein_path = "books/frankenstein.txt"
     book_text = get_book_text(frankenstein_path)
     word_count = count_words(book_text)
+    char_count = count_characters(book_text)
     print(book_text)
     print(word_count)
+    print(char_count)
 
 
 def count_words(text):
@@ -14,5 +16,14 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def count_characters(string):
+    string = string.lower()
+    letters = set(string)
+    count_dict = {}
+
+    for letter in letters:
+        count_dict[letter] = string.count(letter)
+    
+    return count_dict
 
 main()
