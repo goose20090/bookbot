f383ac4d12afbae8272fbc1b52527f58cda92a95
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2025-01-26 12:32:07 +0000

    finish formatting final report- sort char counts by count and print accordingly

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ def main():
     frankenstein_path = "books/frankenstein.txt"
     book_text = get_book_text(frankenstein_path)
     word_count = count_words(book_text)
-    char_count = count_characters(book_text)
+    char_counts = count_characters(book_text)
+    sorted_char_counts = sort_by_counts(char_counts)
     print("--- Begin report of books/frankenstein.txt ---")
     print(f"{word_count} words found in the document")
+
+    for char_dict in sorted_char_counts:
+        print(f"The '{char_dict["char"]}' character was found {char_dict["count"]} times")
 
 
 
@@ -19,11 +23,22 @@ def get_book_text(path):
 def count_characters(string):
     string = string.lower()
     letters = set(string)
-    count_dict = {}
+    count_list = []
 
     for letter in letters:
-        count_dict[letter] = string.count(letter)
+        if letter.isalpha():
+            count_list.append({"char": letter, "count": string.count(letter)})
     
-    return count_dict
+    return count_list
+    
+
+def sort_on(dict):
+    return dict["count"]
+
+def sort_by_counts(count_list):
+    count_list.sort(reverse = True, key=sort_on)
+    return count_list
+
+
 
 main()
